75554ae0678beb39044ce919a956031680f7503a
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-06-07 11:24:49 +0100

    hjkl

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,10 +4,10 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Sarah and Dave</h1>
+                <h1>17 July 2021</h1>
             </header>
             <div className="content">
-                <p>17 JULY 2021</p>
+                <p>Secret Garden Marquee, Burwash Manor, Barton</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
