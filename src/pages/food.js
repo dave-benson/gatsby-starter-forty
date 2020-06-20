ee9029074c3b80def826a7a5cc4cf6a07126c1a5
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-06-20 18:16:49 +0100

    bannerland

--- src/pages/food.js
+++ src/pages/food.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
-import BannerLanding from '../components/BannerFood'
+import BannerLanding from '../components/BannerLanding'
 
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
@@ -14,7 +14,7 @@ const Landing = (props) => (
             <meta name="description" content="Landing Page" />
         </Helmet>
 
-        <BannerFood />
+        <BannerLanding />
 
         <div id="main">
             <section id="one">
