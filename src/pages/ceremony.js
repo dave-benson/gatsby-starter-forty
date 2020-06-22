11da9375b68228c62946beb8f1f59905a06bc802
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-06-22 10:13:39 +0100

    cere

--- src/pages/ceremony.js
+++ src/pages/ceremony.js
@@ -19,7 +19,7 @@ const Generic = (props) => (
                     </header>
                     <span className="image main"><img src={pic11} alt="" /></span>
     <div className="grid-wrapper">   
-	<div className="col-4">
+	<div className="col-6">
 				<h4>Timings</h4>
                       <ul className="alt">
 						<li>Arrive		14:00</li>
@@ -29,23 +29,24 @@ const Generic = (props) => (
 						<li>Finish		24:00</li>
                     </ul>
 	</div>
-	<div className="col-4">
+	<div className="col-6">
 				<h4>Dress Code</h4>
                       <ul className="alt">
 						<li>smart casual/dressy casual</li>
                       	<li>ties optional</li>
                     </ul>
 	</div>
+	<h4>Other Information</h4>
 	<div className="col-4">
-				<h4>Other Information</h4>
-                      <p>The wedding breakfast will start at 5pm, please have lunch before if you'll get hungry</p>
+<p>The wedding breakfast will start at 5pm, please have lunch before if you'll get hungry</p>
+	</div>
+	<div className="col-4">
 <p>If you would like to bring confetti, only biodegradable petal confetti is allowable by the venue</p>
+	</div>
+	<div className="col-4">
 <p>To allow all guests to relax and enjoy themselves, we have chosen to make our special day adults only. We thank you for your understanding</p>
 	</div>
-	
-	
 	</div>
-                    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
                 </div>
             </section>
         </div>
