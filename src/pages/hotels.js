046686b906fea06356bd30be1e4473d014387249
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-06-22 19:04:38 +0100

    kujyhtgrf

--- src/pages/hotels.js
+++ src/pages/hotels.js
@@ -62,39 +62,39 @@ const Generic = (props) => (
 	</ul>
 	</div>
 	</div>
-	 <h3>Taxis</h3>	
-<div className="grid-wrapper">
-	<div className="col-4"><span className="image fit"><img src={pic08} alt="" /></span></div>
-	</div>
-	<div className="col-4">
+	 <h3>Taxis</h3>
+<div className="grid-wrapper">	
+<div className="col-4"><span className="image fit"><img src={pic08} alt="" /></span></div>
+	</div>	
+<div className="box alt">
+             <div className="grid-wrapper">	
+	<div className="col-6">
 	<div className="box">
 	<h4>Panther</h4>
-                                        <ul className="alt">
-                                            <li><a href="https.pathertaxis.co.uk" target="_blank">Website</a></li>
-                                            <li>01223 715715</li>
-											<li>
-	<ul className="icons">
-                                            <li><a href="https://play.google.com/store/apps/details?id=uk.riide.panther&hl=en_GB" target="_blank" className="icon fab fa-google"><span className="label">Google Play</span></a></li>
-                                            <li><a href="https://apps.apple.com/us/app/panther-taxis/id1240884516?ign-mpt=uo4" target="_blank" className="icon fab fa-apple"><span className="label">Apple</span></a></li>
-                                        </ul>
-	</li>
-                                        </ul>
-	
-					</div>
-	</div>
-	<div className="col-4">
+	                                <ul className="actions">
+                                    <li><a href="https://play.google.com/store/apps/details?id=uk.riide.panther&hl=en_GB" target="_blank" className="button special icon fab fa-google">Android</a></li>
+                                    <li><a href="https://apps.apple.com/us/app/panther-taxis/id1240884516?ign-mpt=uo4" target="_blank" className="button icon fab fa-apple">Apple</a></li>
+                                </ul>
+							<p><a href="https.pathertaxis.co.uk" target="_blank">Website</a>   01223 715715</p>
+</div>	
+</div>	
+
+	<div className="col-6">
 	<div className="box">
 	<h4>Uber</h4>
-
-	<ul className="icons">
-                                            <li><a href="https://play.google.com/store/apps/details?id=com.ubercab&hl=en_GB" target="_blank" className="icon fab fa-google"><span className="label">Google Play</span></a></li>
-                                            <li><a href="https://apps.apple.com/us/app/uber/id368677368" target="_blank" className="icon fab fa-apple"><span className="label">Apple</span></a></li>
-   
-                                        </ul>
+	                                <ul className="actions">
+                                    <li><a href="https://play.google.com/store/apps/details?id=com.ubercab&hl=en_GB" target="_blank" className="button special icon fab fa-google">Android</a></li>
+                                    <li><a href="https://apps.apple.com/us/app/panther-taxis/id1240884516?ign-mpt=uo4" target="_blank" className="button icon fab fa-apple">Apple</a></li>
+                                </ul>
+</div>	
+</div>	
 	
-					</div>	
+</div>	
+</div>	
+
 	</div>	
-	</div>
+	
+
 
 	
 	
