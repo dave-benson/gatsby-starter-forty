f25724f9585707756f9014b9b00d0af461f23533
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-06-22 15:58:16 +0100

    sites

--- src/pages/hotels.js
+++ src/pages/hotels.js
@@ -35,6 +35,7 @@ const Generic = (props) => (
                                             <li>Newmarket Road</li>
                                             <li>Cambridge</li>
                                             <li>CB1 3EP</li>
+		<li><a href="https://www.premierinn.com/gb/en/hotels/england/cambridgeshire/cambridge/cambridge-city-east.html?cid=GLBC_CAMNEW" className="button special fit small fas fa-external-link-alt" target="_blank">Website</a></li>
                                         </ul>
 	</div>
 <div className="col-3">
@@ -43,14 +44,14 @@ const Generic = (props) => (
                                             <li>180-190 Newmarket Road</li>
                                             <li>Cambridge</li>
                                             <li>CB5 8HF</li>
+		<li><a href="https://www.travelodge.co.uk/search/results?location=Cambridge&lat=&long=&action=search&source=l&checkIn=&rooms%5B0%5D%5Badults%5D=1&rooms%5B0%5D%5Bchildren%5D=0&gclid=EAIaIQobChMIld2YttHp4wIVh7HtCh2fDA3WEAAYASAAEgJEEvD_BwE" className="button special fit small fas fa-external-link-alt" target="_blank">Website</a></li>
 	</ul>
 	</div>
 <div className="col-3">
 		<h4>More ...</h4>
                                         <ul className="alt">
-                                            <li>Search for</li>
-                                            <li>more hotels</li>
-                                            <li>in Cambridge</li>
+		<li><a href="https://www.trivago.co.uk/?aDateRange%5Barr%5D=2021-07-17&aDateRange%5Bdep%5D=2021-07-18&aPriceRange%5Bfrom%5D=0&aPriceRange%5Bto%5D=0&iRoomType=7&aRooms%5B0%5D%5Badults%5D=2&cpt2=16802%2F200&iViewType=1&bIsSeoPage=0&sortingId=1&slideoutsPageItemId=&iGeoDistanceLimit=16093&address=&addressGeoCode=&offset=0&ra=" className="button special fit small fas fa-external-link-alt" target="_blank">Search for more</a></li>
+                                        
 	</ul>
 	</div>
 	</div>
