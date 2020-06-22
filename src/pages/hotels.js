f311b02590cfe0816b770edbc4138c88fbc19c54
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-06-22 15:45:12 +0100

    button

--- src/pages/hotels.js
+++ src/pages/hotels.js
@@ -20,33 +20,36 @@ const Generic = (props) => (
                     <span className="image main"><img src={pic11} alt="" /></span>
     <div className="grid-wrapper">   
 	<div className="col-3">
-				<h4>Gonville</h4>
-                                <p>Gonville Place</p>
-									<p>Cambridge</p>
-									<p>Cambridgeshire</p>
-									<p>CB1 1LY</p>
+	<h4>Gonville</h4>
+                                        <ul className="alt">
+                                            <li>Gonville Place</li>
+                                            <li>Cambridge</li>
+                                            <li>CB1 1LY</li>
+	<li><a href="https://www.gonvillehotel.co.uk/" className="button special fit small fas fa-map-marker" target="_blank">Google Maps</a></li>
+                                        </ul>
 	
 					</div>
 	<div className="col-3">
-				<h4>Premier Inn</h4>
-                                <p>Newmarket Road</p>
-									<p>Cambridge</p>
-									<p>Cambridgeshire</p>
-									<p>CB1 3EP</p>
+		<h4>Premier Inn</h4>
+                                        <ul className="alt">
+                                            <li>Newmarket Road</li>
+                                            <li>Cambridge</li>
+                                            <li>CB1 3EP</li>
+                                        </ul>
 	</div>
 <div className="col-3">
-				<h4>Gonville3</h4>
-                                <p>Gonville Place</p>
-									<p>Cambridge</p>
-									<p>Cambridgeshire</p>
-									<p>CB1 1LY</p>
+		<h4>Travelodge</h4>
+                                        <ul className="alt">
+                                            <li>180-190 Newmarket Road</li>
+                                            <li>Cambridge</li>
+                                            <li>CB5 8HF</li>
 	</div>
 <div className="col-3">
-				<h4>Gonville4</h4>
-                                <p>Gonville Place</p>
-									<p>Cambridge</p>
-									<p>Cambridgeshire</p>
-									<p>CB1 1LY</p>
+		<h4>More ...</h4>
+                                        <ul className="alt">
+                                            <li>Search for</li>
+                                            <li>more hotels</li>
+                                            <li>in Cambridge</li>
 	</div>
 	</div>
 	 
