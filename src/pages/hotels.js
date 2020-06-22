b6c42f810676b071c531b6ace6127fa89bfbf0ba
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-06-22 19:27:21 +0100

    999

--- src/pages/hotels.js
+++ src/pages/hotels.js
@@ -72,7 +72,7 @@ const Generic = (props) => (
                                     <li><a href="https://play.google.com/store/apps/details?id=uk.riide.panther&hl=en_GB" target="_blank" className="button special icon fab fa-google">Android</a></li>
                                     <li><a href="https://apps.apple.com/us/app/panther-taxis/id1240884516?ign-mpt=uo4" target="_blank" className="button special icon fab fa-apple">Apple</a></li>
                                 </ul>
-							<p><a href="https.pathertaxis.co.uk" target="_blank">Website</a>   01223 715715</p>
+							<p><a href="https.pathertaxis.co.uk" target="_blank">Website</a>&nbsp;&nbsp;&nbsp;&nbsp;01223 715715</p>
 </div>	
 </div>	
 
