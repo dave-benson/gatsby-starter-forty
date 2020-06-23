5218c565c47d44570fb8b46c490528cf6ba6aa1d
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-06-23 11:22:46 +0100

    123

---
+++ src/pages/soon.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <div id="main" className="alt">
+        <section id="one">
+            <div className="inner">
+                <h1>Coming soon</h1>
+                <p>Be patient, we'll have all the information soon ...</p>
+            </div>
+        </section>
+    </div>
+  </Layout>
+)
+
+export default NotFoundPage
