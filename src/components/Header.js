6f1c5c4d30ab0baaacd63c51d3183df1a1951f5b
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-12-21 21:43:42 +0000

    head

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Sarah and Dave</strong> <h4>17 July 21</h4></Link>
+        <Link to="/" className="logo"><strong>Sarah and Dave</strong> <h6>17 July 21</h6></Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
