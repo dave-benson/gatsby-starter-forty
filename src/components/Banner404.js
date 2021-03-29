746fd9c738d276b5b8c79bc96eb845cd72daad3b
davidrossbenson85 <davidrossbenson85@googlemail.com>
2021-03-29 14:28:01 +0100

    replan

---
+++ src/components/Banner404.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BannerFood = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Food</h1>
+            </header>
+            <div className="content">
+                <p>Food will be served at 17:00<br />
+                Please let us know on the RSVP if you have any allergies/intolerances</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerFood
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,83 +1,17 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import Helmet from 'react-helmet'
 import Layout from '../components/layout'
-import Banner from '../components/Banner'
 
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
+const NotFoundPage = () => (
+  <Layout>
+    <div id="main" className="alt">
+        <section id="one">
+            <div className="inner">
+                <h1>NOT FOUND</h1>
+                <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            </div>
+        </section>
+    </div>
+  </Layout>
+)
 
-class HomeIndex extends React.Component {
-    render() {
-
-        return (
-            <Layout>
-                <Helmet
-                    title="Sarah & Dave - Home"
-                    meta={[
-                        { name: 'description', content: 'Sample' },
-                        { name: 'keywords', content: 'sample, something' },
-                    ]}
-                >
-                </Helmet>
-
-                <Banner />
-
-                <div id="main">
-                    <section id="one" className="tiles">
-                        <article style={{backgroundImage: `url(${pic01})`}}>
-                            <header className="major">
-                                <h3>Ceremony</h3>
-                                <p></p>
-                            </header>
-                            <Link to="/ceremony" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic02})`}}>
-                            <header className="major">
-                                <h3>Hotels and Taxis</h3>
-                                <p></p>
-                            </header>
-                            <Link to="/hotels" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic03})`}}>
-                            <header className="major">
-                                <h3>Food</h3>
-                                <p></p>
-                            </header>
-                            <Link to="/food" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic04})`}}>
-                            <header className="major">
-                                <h3>Gifts</h3>
-                                <p></p>
-                            </header>
-                            <Link to="/gifts" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic05})`}}>
-                            <header className="major">
-                                <h3>RSVP</h3>
-                                <p></p>
-                            </header>
-                            <Link to="/rsvp" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic06})`}}>
-                            <header className="major">
-                                <h3>Travel & Parking</h3>
-                                <p></p>
-                            </header>
-                            <Link to="/travel" className="link primary"></Link>
-                        </article>
-                    </section>
-                 
-                </div>
-
-            </Layout>
-        )
-    }
-}
-
-export default HomeIndex
+export default NotFoundPage
